a4d350096da61b60b0ae9dcc1858f061f1627bc1
Jani Matkala <Jani.Matkala@opuscapita.com>
2019-10-22 14:30:57 +0300

    update register.js

--- test/register.js
+++ test/register.js
@@ -1,5 +1,8 @@
-require('babel-register')({
-  ignore: /node_modules/,
-});
+// By default all requires to node_modules will be ignored
+// https://babeljs.io/docs/en/babel-register#ignores-node-modules-by-default
+require('@babel/register')();
+require('raf/polyfill');
 require('ignore-styles');
-require('global-jsdom')();
+require('global-jsdom')(undefined, {
+  url: 'http://localhost',
+});
